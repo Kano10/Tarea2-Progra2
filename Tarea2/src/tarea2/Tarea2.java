7b467fc21eeb7ce602df1f6eef5fff47bdbdc315
Kano10 <canomga@gmail.com>
2022-10-26 15:36:17 -0300

    cosas de netbeans

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -3,9 +3,14 @@ package tarea2;
 
 
 public class Tarea2 {
-
+ enum Bebidass{
+          cocacola, fanta;
+        }
     public static void main(String[] args) {
-        
+       Bebidass deposito;
+       deposito=Bebidass.cocacola;
+       deposito=Bebidass.fanta;
+       Bebidass.fanta=10;
     }
     
 }
