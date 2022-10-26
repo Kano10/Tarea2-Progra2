bf277ca253b7a87f580e6ab342dc8a67de8c43a4
Lirayen Martinez <lirmartinez2021@udec.cl>
2022-10-26 15:52:29 -0300

    Avance en monedas

--- Tarea2/src/tarea2/Moneda.java
+++ Tarea2/src/tarea2/Moneda.java
@@ -1,14 +1,34 @@
 package tarea2;
 
 public abstract class Moneda {
-    
+
+    public Moneda getSerie() {
+        return this;
+    }
+
+    public abstract int getValor();
 }
-class M50 extends Moneda{
-    
+
+class M500 extends Moneda {
+
+    @Override
+    public int getValor() {
+        return 500;
+    }
 }
-class M100 extends Moneda{
-    
+
+class M100 extends Moneda {
+
+    @Override
+    public int getValor() {
+        return 100;
+    }
 }
-class M1000 extends Moneda{
-    
+
+class M1000 extends Moneda {
+
+    @Override
+    public int getValor() {
+        return 1000;
+    }
 }
